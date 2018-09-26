6337e2bc47bcf0203a5d76e0f04b8f2d4d756185
Joao Sinieghi <jpsinieghi@gmail.com>
2018-09-26 15:58:57 -0300

    atualizacao 1

--- legenda2.py
+++ legenda2.py
@@ -8,18 +8,30 @@ def legenda_gcs(gcs_uri):
     config = types.RecognitionConfig(
         encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
         sample_rate_hertz=44100,
-        language_code='pt-BR',
-		enableWordTimeOffsets:'true')
+        language_code='pt-BR')
+		
 
     operation = client.long_running_recognize(config, audio)
 
     print('Esperando a conclusao da operacao...')
     response = operation.result(timeout=3000)
-    file = open('c:\legenda.txt','w')
+    file = open('c:\legenda2.txt','w')
+    
     for result in response.results:
-        #print(u'Legenda: {}'.format(result.alternatives[0].transcript))
-        #print('Acuracia: {}'.format(result.alternatives[0].confidence))
+     	#print(u'Legenda: {}'.format(result.alternatives[0].transcript))
+    	#print('Acuracia: {}'.format(result.alternatives[0].confidence))
         file.write('{}\n'.format(result.alternatives[0].transcript))
+    
     file.close()
 	
-#gsutil acl ch -u AllUsers:R "gs://legenda/mono02.flac"
+	
+#ffmpeg.exe -i "flac1.flac" -ac 1 mono1.flac
+
+	
+#gsutil acl ch -u AllUsers:R "gs://legenda/mono02.flac" para deixar arquivo publico no Storage
+#set GOOGLE_APPLICATION_CREDENTIALS = c:\MyProject.json
+
+##Ambiente de desenvolvimento do Python##
+#cd your-project
+#virtualenv --python python3 env
+#.\env\Scripts\activate
--- legenda_api.py
+++ legenda_api.py
@@ -1,13 +1,46 @@
-from google.cloud import speech_v1
-from google.cloud.speech_v1 import enums
+#https://googlecloudplatform.github.io/google-cloud-python/latest/speech/index.html
+#https://cloud.google.com/speech-to-text/docs/basics
 
-client = speech_v1.SpeechClient()
+def legenda_gcs(gcs_uri):
+	from google.cloud import speech_v1
+	from google.cloud.speech_v1 import enums
+	#import os
+	#os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/path/to/file.json"
+	
+	client = speech_v1.SpeechClient()
 
-encoding = enums.RecognitionConfig.AudioEncoding.FLAC
-sample_rate_hertz = 44100
-language_code = 'pt-BR'
-config = {'encoding': encoding, 'sample_rate_hertz': sample_rate_hertz, 'language_code': language_code}
-uri = 'gs://legenda/mono02.flac'
-audio = {'uri': uri}
+	encoding = enums.RecognitionConfig.AudioEncoding.FLAC
+	sample_rate_hertz = 44100
+	language_code = 'pt-BR'
+	config = {'encoding': encoding, 'sample_rate_hertz': sample_rate_hertz, 'language_code': language_code}
+	#uri = 'gs://legenda/mono02.flac'
+	audio = {'uri': gcs_uri}
 
-response = client.recognize(config, audio)
+	operation = client.long_running_recognize(config, audio)
+	print('Esperando a conclusao da operacao API...')
+	response = client.recognize(config, audio)
+	
+	file = open('c:\legenda_api.txt','w')
+	for result in response.results:
+		file.write('{}\n'.format(result.alternatives[0].transcript))
+		
+	file.close()
+	
+def implicit():
+    from google.cloud import storage
+
+    # If you don't specify credentials when constructing the client, the
+    # client library will look for credentials in the environment.
+    storage_client = storage.Client()
+
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
+	
+	
+	
+	
+	#for result in response.results:
+	#    file.write('{}\n'.format(result.alternatives[0].transcript))
+    #file.close()
+
